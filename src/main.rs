166a9e607b68a59851aed70c8ff446340afaf2e1
Jonathan Nilsson <lord.jonil@gmail.com>
2016-03-12 19:02:21 +0100

    Not working

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate cgmath;
 extern crate rand;
 
 use cgmath::*;
+use glium::VertexBuffer;
 use glium::{DisplayBuild, Surface};
 use rand::distributions::{IndependentSample, Range};
 
@@ -55,6 +56,52 @@ fn simulate_particles(particles: &mut [Particle], dt: f32)
     }
 }
 
+#[derive(Copy, Clone, Default)]
+struct Vertex {
+    pos: [f32; 3],
+    tex: [f32; 2],
+}
+
+implement_vertex!(Vertex, pos, tex);
+
+fn build_vertex_buffer(particles: &[Particle], vertices: &mut [Vertex]) {
+
+    assert!(particles.len() == vertices.len() / 4);
+
+    for i in 0..particles.len() {
+
+        vertices[i*4 + 0].pos[0] = -particles[i].size / 2.0 + particles[i].pos.x;
+        vertices[i*4 + 0].pos[1] = -particles[i].size / 2.0 + particles[i].pos.y; 
+        vertices[i*4 + 0].pos[2] = 0.0;
+
+        vertices[i*4 + 1].pos[0] = particles[i].size / 2.0 + particles[i].pos.x;
+        vertices[i*4 + 1].pos[1] = -particles[i].size / 2.0 + particles[i].pos.y;
+        vertices[i*4 + 1].pos[2] = 0.0;
+
+        vertices[i*4 + 2].pos[0] = particles[i].size / 2.0 + particles[i].pos.x;
+        vertices[i*4 + 2].pos[1] = particles[i].size / 2.0 + particles[i].pos.y;
+        vertices[i*4 + 2].pos[2] = 0.0;
+
+        vertices[i*4 + 3].pos[0] = -particles[i].size / 2.0 + particles[i].pos.x;
+        vertices[i*4 + 3].pos[1] = particles[i].size / 2.0 + particles[i].pos.y;
+        vertices[i*4 + 3].pos[2] = 0.0;
+
+        vertices[i*4 + 0].tex[0] = 0.0;
+        vertices[i*4 + 0].tex[1] = 0.0;
+
+        vertices[i*4 + 1].tex[0] = 1.0;
+        vertices[i*4 + 1].tex[1] = 0.0;
+
+        vertices[i*4 + 2].tex[0] = 1.0;
+        vertices[i*4 + 2].tex[1] = 1.0;
+
+        vertices[i*4 + 3].tex[0] = 0.0;
+        vertices[i*4 + 3].tex[1] = 1.0;
+    }
+}
+
+const PARTICLE_COUNT: i32 = 1000;
+
 fn main() {
 
     let display = glium::glutin::WindowBuilder::new()
@@ -62,12 +109,64 @@ fn main() {
         .with_dimensions(WIDTH as u32, HEIGHT as u32)
         .build_glium().unwrap();
 
-    let particles: Vec<Particle> = vec![Default::default(); 1000];
+    let program = program!(&display,
+        130 => {
+            vertex: "
+                #version 130
+                
+                attribute vec3 vertex_pos;
+                attribute vec2 vertex_tex;
+
+                varying vec3 pos;
+                varying vec2 tex;
+
+                void main() {
+                    pos = vertex_pos;
+                    tex = vertex_tex;
+                    gl_Position = vec4(vertex_pos, 1.0);
+                }
+            ",
+
+            fragment: "
+                #version 130
+                
+                varying vec3 pos;
+                varying vec2 tex;
+
+                void main()
+                {
+                    vec3 color = vec3(191.0/255.0, 0.0, 1.0);
+
+                    vec2 local = (tex - 0.5) * 2.0;
+
+                    float r = sqrt(local.x*local.x + local.y*local.y);
+                    r = clamp(r, 0.0, 1.0);
+                    float alpha = 1.5;
+                    alpha*= pow(1.0 - r, 2.0);
+
+                    gl_FragColor = vec4(color, alpha);
+                }
+            ",
+        },
+    ).unwrap();
+
+    let mut particles: Vec<Particle> = vec![Default::default(); PARTICLE_COUNT as usize];
+
+    let mut vertices: Vec<Vertex> = vec![Default::default(); 4 * PARTICLE_COUNT as usize];
+    let vertex_buffer: VertexBuffer<Vertex> = VertexBuffer::dynamic(&display, &[Default::default(); 4 * PARTICLE_COUNT as usize]).unwrap();
 
     loop {
 
+        simulate_particles(&mut particles, 1.0 / 60.0);
+
+        build_vertex_buffer(&particles, &mut vertices);
+
+        vertex_buffer.write(&vertices);
+
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 0.2, 1.0);
+        target.draw(&vertex_buffer, glium::index::NoIndices, &program, &glium::uniforms::EmptyUniforms,
+            &Default::default()).unwrap();
         target.finish().unwrap();
 
         for ev in display.poll_events() {
